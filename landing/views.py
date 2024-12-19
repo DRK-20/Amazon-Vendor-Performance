41c0f4a62a705bd746da0598a49fb06ec0d4b496
Dhruv R Krishnan <dhruvrkrishnan@gmail.com>
2024-12-19 15:59:51 +0530

    front end touch ups

--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
 def landing_page(request):
-    """
-    Render the landing page for users who are not logged in.
-    """
     return render(request, 'landing/landing_page.html')
--- registrations/models.py
+++ registrations/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class RegisteredProduct(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='registered_products')
-    asin = models.CharField(max_length=20, unique=True)  # Enforce uniqueness
+    asin = models.CharField(max_length=20, unique=True)
     product_name = models.CharField(max_length=255)
     average_rating = models.CharField(max_length=10, null=True, blank=True)
     rating_breakdown = models.JSONField(null=True, blank=True)
